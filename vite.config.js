3c5fecd70bf360f0a67550a433cd1a95db344188
VIJAY071102 <jsvijayjsv@gmail.com>
2025-01-05 23:48:07 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { ghPages } from "vite-plugin-gh-pages";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: "/Shcool-management-system/",
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 KB
   },
